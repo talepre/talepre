fcdf9116a7028686e5f0fb5e2953551046836901
Tale Prestmo <tale.prestmo@gmail.com>
2019-12-18 15:20:56 +0100

    Basic start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import img from './cacti.jpg'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img className='background-pic' src={img} alt='backgroundpic' />
+      <section className='site-text'>
+        <h1>talepre</h1>
+        <h2>developer</h2>
+      </section>
     </div>
   );
 }
